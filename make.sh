d8eb6795cda94d43741029db109c4f10c912afac
eduard <eduard@MacBook-Pro-di-Eduard.local>
2017-06-29 10:34:54 +0200

    removed old

--- make.sh
+++ make.sh
@@ -11,10 +11,10 @@ language_model="$(./scripts/get_path.sh 'concept_language.lm')"
 
 sentences="$(./scripts/get_path.sh 'test_sentences')"
 
-test3="$(./scripts/get_path.sh 'results')"/test3
-mkdir $test3
+test_word_concept="$(./scripts/get_path.sh 'results')"/test_word_concept
+mkdir $mtest
 
-result=$(scripts/Test3/create_transducer_word_concept.sh)
+result=$(scripts/test_word_concept/create_transducer_word_concept.sh)
 transducer_word_concept="$(scripts/get_path.sh "$result")"
 echo $transducer_word_concept
 
@@ -23,31 +23,31 @@ echo $result
 transducer_word_concept_fst="$(./scripts/get_path.sh "$result")"
 echo $transducer_word_concept_fst
 
-echo -n "" > ${test3}/result.print
+echo -n "" > ${test_word_concept}/result.print
 
-outline=${test3}/"line"$$
+outline=${test_word_concept}/"line"$$
 while read line
 do
 	echo "sentence: $line"
 
 	./scripts/compile_string.sh "$line" ${outline}.fst $lex
 	echo "compose string to transducer"
-	result=$(./scripts/compose_transducer.sh ${outline}.fst ${transducer_word_concept_fst} ${test3} $lex)
+	result=$(./scripts/compose_transducer.sh ${outline}.fst ${transducer_word_concept_fst} ${test_word_concept} $lex)
 	echo "compose with language model"
-	result2=$(./scripts/compose_transducer.sh ${result}.fst ${language_model} ${test3} $lex)
+	result2=$(./scripts/compose_transducer.sh ${result}.fst ${language_model} ${test_word_concept} $lex)
 
-	cat ${result2}.fst | fstshortestpath > ${test3}/shortestpath.fst
-	fstprint -isymbols=$lex -osymbols=$lex ${test3}/shortestpath.fst | sort -r -k1 -n >> ${test3}/result.print
+	cat ${result2}.fst | fstshortestpath > ${test_word_concept}/shortestpath.fst
+	fstprint -isymbols=$lex -osymbols=$lex ${test_word_concept}/shortestpath.fst | sort -r -k1 -n >> ${test_word_concept}/result.print
 
 done <  $sentences
 
 rm ${outline}*
-rm ${test3}/shortestpath.fst
-cp $test_data ${test3}/test_data
-tmp=${test3}/"eval"$$
-awk '{tag=substr($4,0,1); if (tag == "O") {print $3, tag} else {print $3, $4}}' ${test3}/result.print > $tmp
-mv $tmp ${test3}/result.print
+rm ${test_word_concept}/shortestpath.fst
+cp $test_data ${test_word_concept}/test_data
+tmp=${test_word_concept}/"eval"$$
+awk '{tag=substr($4,0,1); if (tag == "O") {print $3, tag} else {print $3, $4}}' ${test_word_concept}/result.print > $tmp
+mv $tmp ${test_word_concept}/result.print
 
-awk 'FNR==NR { a[FNR""] = $1 " " $2; next } { print a[FNR""] " " $2 }' ${test3}/test_data ${test3}/result.print > $tmp 
-mv $tmp ${test3}/to_eval
+awk 'FNR==NR { a[FNR""] = $1 " " $2; next } { print a[FNR""] " " $2 }' ${test_word_concept}/test_data ${test_word_concept}/result.print > $tmp 
+mv $tmp ${test_word_concept}/to_eval
 
